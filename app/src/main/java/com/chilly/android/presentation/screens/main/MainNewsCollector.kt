45a4ff9a480f48763c4915d32dbabc58facff9bc
avmushtukov <mushtukov.artem@mail.ru>
2025-02-20 22:25:01 +0300

    app-12: use string resources in main screen

--- app/src/main/java/com/chilly/android/presentation/screens/main/MainNewsCollector.kt
+++ app/src/main/java/com/chilly/android/presentation/screens/main/MainNewsCollector.kt
@@ -1,8 +1,8 @@
 package com.chilly.android.presentation.screens.main
 
 
+import com.chilly.android.data.remote.dto.QuizType
 import com.chilly.android.presentation.navigation.Destination
-import com.chilly.android.presentation.screens.quiz.QuizType
 import com.github.terrakok.cicerone.Router
 import kotlinx.coroutines.flow.FlowCollector
 import javax.inject.Inject
@@ -15,7 +15,7 @@ class MainNewsCollector @Inject constructor(
         when(value) {
             MainNews.NavigateLogin -> router.newRootScreen(Destination.LogIn)
             MainNews.NavigateOnboarding -> router.navigateTo(Destination.Onboarding())
-            MainNews.NavigateMainQuiz -> router.navigateTo(Destination.Quiz(QuizType.MAIN))
+            MainNews.NavigateMainQuiz -> router.navigateTo(Destination.Quiz(QuizType.BASE))
             MainNews.NavigateShortQuiz -> router.navigateTo(Destination.Quiz(QuizType.SHORT))
         }
     }
