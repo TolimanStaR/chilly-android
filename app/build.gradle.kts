d26b208989c053cedbbcf71d7464341237600505
avmushtukov <mushtukov.artem@mail.ru>
2025-04-19 21:36:24 +0300

    add signing config disable splashScreenTests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,15 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        create("release") {
+            storeFile =
+                file("${rootDir}/chilly-keystore.jks")
+            storePassword = System.getenv("STORE_PASSWORD")
+            keyAlias = "release_key"
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
     namespace = "com.chilly.android"
     compileSdk = 35
 
@@ -28,6 +37,7 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
