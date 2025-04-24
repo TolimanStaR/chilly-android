109e16110d492e472d4886acd4ebf222cdee4b2e
avmushtukov <mushtukov.artem@mail.ru>
2025-04-24 22:56:27 +0300

    app-18 fix snackbar crash

--- app/src/main/java/com/chilly/android/presentation/screens/forgot_password/ForgotPasswordNewsCollector.kt
+++ app/src/main/java/com/chilly/android/presentation/screens/forgot_password/ForgotPasswordNewsCollector.kt
@@ -4,6 +4,7 @@ import androidx.annotation.StringRes
 import androidx.compose.material3.SnackbarHostState
 import com.chilly.android.R
 import com.chilly.android.presentation.common.structure.ResourcesHolder
+import com.chilly.android.presentation.common.structure.SnackbarShower
 import com.chilly.android.presentation.common.structure.getString
 import com.chilly.android.presentation.navigation.Destination
 import com.github.terrakok.cicerone.Router
@@ -12,22 +13,18 @@ import javax.inject.Inject
 
 class ForgotPasswordNewsCollector @Inject constructor(
     private val router: Router,
-    private val snackbarHostState: SnackbarHostState,
-    private val resourcesHolder: ResourcesHolder
+    private val snackbarShower: SnackbarShower
 ) : FlowCollector<ForgotPasswordNews> {
 
     override suspend fun emit(value: ForgotPasswordNews) {
         when(value) {
             ForgotPasswordNews.NavigateLogin -> router.backTo(Destination.LogIn)
             ForgotPasswordNews.NavigateSignUp -> router.replaceScreen(Destination.SignUp)
-            ForgotPasswordNews.FailedRequest -> showSnackbar(R.string.general_fail_message)
-            ForgotPasswordNews.PasswordChanged -> showSnackbar(R.string.password_changed)
-            ForgotPasswordNews.WrongCode -> showSnackbar(R.string.wrong_code)
-            ForgotPasswordNews.WrongEmail -> showSnackbar(R.string.wrong_code)
+            ForgotPasswordNews.FailedRequest -> snackbarShower.show(R.string.general_fail_message)
+            ForgotPasswordNews.PasswordChanged -> snackbarShower.show(R.string.password_changed)
+            ForgotPasswordNews.WrongCode -> snackbarShower.show(R.string.wrong_code)
+            ForgotPasswordNews.WrongEmail -> snackbarShower.show(R.string.wrong_code)
         }
     }
 
-    private suspend fun showSnackbar(@StringRes resId: Int) = snackbarHostState.showSnackbar(getString(resId))
-
-    private fun getString(@StringRes resId: Int): String = resourcesHolder.getString(resId)
 }
--- app/src/main/java/com/chilly/android/presentation/screens/login/LoginNewsCollector.kt
+++ app/src/main/java/com/chilly/android/presentation/screens/login/LoginNewsCollector.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.SnackbarHostState
 import com.chilly.android.R
 import com.chilly.android.di.screens.LoginScope
 import com.chilly.android.presentation.common.structure.ResourcesHolder
+import com.chilly.android.presentation.common.structure.SnackbarShower
 import com.chilly.android.presentation.common.structure.getString
 import com.chilly.android.presentation.navigation.Destination
 import com.github.terrakok.cicerone.Router
@@ -14,18 +15,15 @@ import javax.inject.Inject
 @LoginScope
 class LoginNewsCollector @Inject constructor(
     private val router: Router,
-    private val snackBarHostState: SnackbarHostState,
-    private val resourcesHolder: ResourcesHolder
+    private val snackbarShower: SnackbarShower
 ) : FlowCollector<LoginNews> {
 
     override suspend fun emit(value: LoginNews) {
         when(value) {
             LoginNews.NavigateMain -> router.newRootScreen(Destination.Main)
-            LoginNews.LoginFailed -> snackBarHostState.showSnackbar(getString(R.string.login_failed_snackbar))
+            LoginNews.LoginFailed -> snackbarShower.show(R.string.login_failed_snackbar)
             LoginNews.NavigateSignUp -> router.navigateTo(Destination.SignUp)
             LoginNews.NavigateForgotPassword -> router.navigateTo(Destination.ForgotPassword)
         }
     }
-
-    private fun getString(@StringRes id: Int): String = resourcesHolder.getString(id)
 }
